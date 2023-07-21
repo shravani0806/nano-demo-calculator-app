3cc7fb20108072760d7bd36669f0b9afcb4092a9
shravani kale <kaleshravani123@gmail.com>
2023-07-21 23:12:22 +0530

    four commit

--- server.js
+++ server.js
@@ -1,28 +1,67 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// const PORT = process.env.PORT || 8080;
+
+// const baseUrl = '/calculator'
+
+// app.use(express.json());
+
+// const baseRouter = express.Router();
+
+// baseRouter.get('/greeting', (req, res) => {
+//     return res.send('Hii');
+// });
+
+// baseRouter.post('/add', (req, res) => {
+//     res.json({ "": null });
+// });
+
+
+// baseRouter.post('/subtract', (req, res) => {
+//     res.json({ "": null });
+// });
+
+// app.use(baseUrl, baseRouter);
+// app.listen(PORT, () => {
+//     console.log("Server running at PORT", PORT);
+// });
+
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello world');
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello---Gunjan");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  const { num1, num2 } = req.body;
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).json({ error: "Invalid input. Please provide valid numbers." });
+  }
+  const result = num1 + num2;
+  res.json({ result });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  const { num1, num2 } = req.body;
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).json({ error: "Invalid input. Please provide valid numbers." });
+  }
+  const result = num1 - num2;
+  res.json({ result });
 });
 
 app.use(baseUrl, baseRouter);
+
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
